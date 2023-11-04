4a10b42c797d5674736c1ba1d8b733e6cb2e6989
Pabloglezr8 <pabligoru99@gmail.com>
2023-11-04 15:40:50 +0100

    Corregimos últimos errores

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-let idCount = 6;
+let idCount = 7;
 
 //función que nos permite desplegar el formulario donde se ingresan 
 //las características de nuetro producto y el boton de añadir productopara insertarlo en la tabla
@@ -46,7 +46,7 @@ function addProduct() {
         id: idCount,
         nombre: productName,
         cantidad: parseInt(productQuantity),
-        precio: parseFloat(productPrice)
+        precio: parseFloat(productPrice)  + "€"
     }
 
     // Incrementar el ID para la próxima camiseta
@@ -62,6 +62,95 @@ function addProduct() {
 
     // Actualizar la tabla
     updateTable();
+}
+
+
+updateTable()
+
+function deleteProduct(id) {
+    const index = camisetasArray.findIndex(product => product.id === id);
+    if (index !== -1) {
+        camisetasArray.splice(index, 1); // Elimina el elemento del array
+        updateTable(); // Actualiza la tabla después de la eliminación
+    }
+}
+
+//esta funcion nos permite editar el producto directamente en la tabala
+function editProduct(id, nameCell, quantityCell, priceCell, editButton) {
+    // Habilita la edición de los campos de nombre, cantidad y precio
+    nameCell.contentEditable = true;
+    quantityCell.contentEditable = true;
+    priceCell.contentEditable = true;
+    
+    // Cambia el botón "Editar" a "Guardar Cambios"
+    editButton.innerHTML = "Guardar Cambios";
+    editButton.className = "saveButton"
+    editButton.removeEventListener("click", editProduct);
+    editButton.addEventListener("click", () => {
+        saveProduct(id, nameCell, quantityCell, priceCell, editButton);
+    });
+}
+
+//funcion validar los cambios en las características del producto y guardarlos en el array
+function saveProduct(id, nameCell, quantityCell, priceCell, editButton) {
+    // Guarda los cambios en el array de productos
+    
+    const product = camisetasArray.find(product => product.id === id);
+    if (product) {
+        product.nombre = nameCell.textContent;
+    }
+    
+    const quantity = parseInt(quantityCell.textContent)
+    if (!isNaN(quantity)) {
+        product.cantidad = quantity;
+    } else {
+        alert("Cantidad no válida")
+        return
+    }
+    
+    const price = parseFloat(priceCell.textContent);
+    if (!isNaN(price)) {
+        product.precio = price
+    } else {
+        alert("Precio no válido")
+        return
+    }
+    
+    if (product && quantity && price) {
+        // Deshabilita la edición de los campos
+        nameCell.contentEditable = false;
+        quantityCell.contentEditable = false;
+        priceCell.contentEditable = false;
+        
+        // Cambia el botón de "Guardar Cambios" a "Editar"
+        editButton.innerHTML = "Editar";
+        editButton.className = "tableButton"
+        editButton.removeEventListener("click", saveProduct);
+        editButton.addEventListener("click", () => {
+            editProduct(id, nameCell, quantityCell, priceCell, editButton);
+        });
+    }
+}
+
+
+// Función para buscar un producto por su nombre
+function searchProduct() {
+    const searchInput = document.getElementById("searchInput").value.toLowerCase();
+    
+    const foundProducts = camisetasArray.filter(product => product.nombre.toLowerCase().includes(searchInput));
+
+    if (foundProducts.length > 0) {
+        const productId = foundProducts.map(product => product.id);
+        const productName = foundProducts.map(product => product.nombre);
+        const productQuantity = foundProducts.map(product => product.cantidad);
+        const productPrice = foundProducts.map(product => product.precio);
+        
+        alert(`Productos encontrados:\nId: ${productId}     Nombre: ${productName}     Cantidad ${productQuantity}     Precio: ${productPrice}\n`);
+    } else {
+        alert("No se encontraron productos con ese nombre.");
+    }
+    
+    searchInput.value = "";
 }
 
 function updateTable() {
@@ -102,7 +191,6 @@ function updateTable() {
         delButton.addEventListener("click", () => {
             const id = element.id;
             deleteProduct(id)
-            console.log("Eliminar elemento con ID " + element.id);
         });
 
         editCell.appendChild(editButton);
@@ -117,94 +205,4 @@ function updateTable() {
 
         tableBody.appendChild(row);
     });
-}
-
-updateTable()
-
-function deleteProduct(id) {
-    const index = camisetasArray.findIndex(product => product.id === id);
-    if (index !== -1) {
-        camisetasArray.splice(index, 1); // Elimina el elemento del array
-        updateTable(); // Actualiza la tabla después de la eliminación
-    }
-}
-
-//esta funcion nos permite editar el producto directamente en la tabala
-function editProduct(id, nameCell, quantityCell, priceCell, editButton) {
-    // Habilita la edición de los campos de nombre, cantidad y precio
-    nameCell.contentEditable = true;
-    quantityCell.contentEditable = true;
-    priceCell.contentEditable = true;
-
-    // Cambia el botón "Editar" a "Guardar Cambios"
-    editButton.innerHTML = "Guardar Cambios";
-    editButton.className = "saveButton"
-    editButton.removeEventListener("click", editProduct);
-    editButton.addEventListener("click", () => {
-        saveProduct(id, nameCell, quantityCell, priceCell, editButton);
-    });
-}
-
-//funcion validar los cambios en las características del producto y guardarlos en el array
-function saveProduct(id, nameCell, quantityCell, priceCell, editButton) {
-    // Guarda los cambios en el array de productos
-
-    const product = camisetasArray.find(product => product.id === id);
-    if (isNaN(product)) {
-        product.nombre = nameCell.textContent;
-    }else{
-        alert("Nombre no válido")
-    }
-
-    const quantity = parseInt(quantityCell.textContent)
-    if (!isNaN(quantity)) {
-        product.cantidad = quantity;
-    } else {
-        alert("Cantidad no válida")
-        return
-    }
-
-    const price = parseFloat(priceCell.textContent);
-    if (!isNaN(price)) {
-        product.precio = price;
-    } else {
-        alert("Precio no válido")
-        return
-    }
-
-    if (product && quantity && price) {
-        // Deshabilita la edición de los campos
-        nameCell.contentEditable = false;
-        quantityCell.contentEditable = false;
-        priceCell.contentEditable = false;
-
-        // Cambia el botón de "Guardar Cambios" a "Editar"
-        editButton.innerHTML = "Editar";
-        editButton.className = "tableButton"
-        editButton.removeEventListener("click", saveProduct);
-        editButton.addEventListener("click", () => {
-            editProduct(id, nameCell, quantityCell, priceCell, editButton);
-        });
-    }
-}
-
-
-// Función para buscar un producto por su nombre
-function searchProduct() {
-    const searchInput = document.getElementById("searchInput").value.toLowerCase();
-
-    const foundProducts = camisetasArray.filter(product => product.nombre.toLowerCase().includes(searchInput));
-
-    if (foundProducts.length > 0) {
-        const productId = foundProducts.map(product => product.id);
-        const productName = foundProducts.map(product => product.nombre);
-        const productQuantity = foundProducts.map(product => product.cantidad);
-        const productPrice = foundProducts.map(product => product.precio);
-
-        alert(`Productos encontrados:\nId: ${productId}     Nombre: ${productName}     Cantidad ${productQuantity}     Precio: ${productPrice}\n`);
-    } else {
-        alert("No se encontraron productos con ese nombre.");
-    }
-
-    searchInput.value = "";
 }
--- scripts/productos.js
+++ scripts/productos.js
@@ -3,10 +3,12 @@ const getRandomQuantity = () =>{
 };
 
 const camisetasArray = [
-{id: 1, nombre: "Pablo", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(50.00)},
-{id: 2, nombre: "Daniel", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(40.00)},
-{id: 3, nombre: "Adrian", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(30.00)},
-{id: 4, nombre: "Alfonso", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(20.00)},
-{id: 5, nombre: "David", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(10.00)},
+{id: 1, nombre: "Sarten", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(50.00) + "€"},
+{id: 2, nombre: "Tornillo M4", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(0.50) + "€"},
+{id: 3, nombre: "Cuchillo ", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(10.00) + "€"},
+{id: 4, nombre: "Desatascador", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(15.00) + "€"},
+{id: 5, nombre: "Hoja de Sierra Radial", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(17.00) + "€"},
+{id: 6, nombre: "Cinta Americana", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(5.00) + "€"},
+{id: 7, nombre: "Destornillador Estrella", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(8.00) + "€"},
 ];
 export {camisetasArray};
