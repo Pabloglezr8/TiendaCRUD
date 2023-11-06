67eb492f83d7d8f6653a1352c35cd3fcba0b3fcf
Pabloglezr8 <pabligoru99@gmail.com>
2023-11-06 12:43:01 +0100

    corregimos errores

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 // https://github.com/Pabloglezr8/TiendaCRUD.git
 
 
-import { camisetasArray } from "./productos.js";
+import { objeto } from "./productos.js";
 
 //al ser un archivo module es necesario cargar los botones a utilizar para que se pueda llamar a las funciones
 document.addEventListener("DOMContentLoaded", function () {
@@ -14,6 +14,59 @@ document.addEventListener("DOMContentLoaded", function () {
     addProductButton.addEventListener("click", addProduct);
     searchButton.addEventListener("click", searchProduct);
 });
+
+function updateTable() {
+    const tableBody = document.getElementById("table-body");
+    tableBody.innerHTML = ""; // Limpiar la tabla
+
+    // Recorrer el array de camisetas y agregar cada una a la tabla
+    objeto.forEach(element => {
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const quantityCell = document.createElement("td");
+        const priceCell = document.createElement("td");
+
+        const editCell = document.createElement("td");
+        editCell.className = "cellButton"
+        const editButton = document.createElement("button");
+        editButton.innerHTML = "Editar";
+        editButton.className = "tableButton"
+
+        const delCell = document.createElement("td");
+        delCell.className = "cellButton"
+        const delButton = document.createElement("button");
+        delButton.innerHTML = "Eliminar";
+        delButton.className = "tableButton"
+
+        nameCell.textContent = element.nombre;
+        quantityCell.textContent = element.cantidad;
+        priceCell.textContent = element.precio;
+
+        // Agregar evento para el botón de editar
+        editButton.addEventListener("click", () => {
+            editProduct(element.id, nameCell, quantityCell, priceCell, editButton);
+        });
+
+        // Agregar evento para el botón de eliminar
+        delButton.addEventListener("click", () => {
+            const id = element.id;
+            deleteProduct(id)
+        });
+
+        editCell.appendChild(editButton);
+        delCell.appendChild(delButton);
+
+        row.appendChild(nameCell);
+        row.appendChild(quantityCell);
+        row.appendChild(priceCell);
+        row.appendChild(editCell);
+        row.appendChild(delCell);
+
+        tableBody.appendChild(row);
+    });
+}
+
+updateTable()
 
 
 let idCount = 8;
@@ -46,18 +99,28 @@ function addProduct() {
         return;
     }
 
+    if (productQuantity < 0) {
+        alert("Cantidad no válida")
+        return;
+    }
+
+    if (productPrice < 0) {
+        alert("Precio no válido")
+        return;
+    }
+
     const camiseta = {
         id: idCount,
         nombre: productName,
         cantidad: parseInt(productQuantity),
-        precio: parseFloat(productPrice)  + "€"
+        precio: parseFloat(productPrice) + "€"
     }
 
     // Incrementar el ID para la próxima camiseta
     idCount++;
 
     // Agregar la camiseta al array
-    camisetasArray.push(camiseta);
+    objeto.push(camiseta);
 
     // Limpiar los campos del formulario
     document.getElementById("product-name").value = "";
@@ -69,27 +132,27 @@ function addProduct() {
 }
 
 
-updateTable()
-
 function deleteProduct(id) {
-    const index = camisetasArray.findIndex(product => product.id === id);
+    const index = objeto.findIndex(product => product.id === id);
     if (index !== -1) {
-        camisetasArray.splice(index, 1); // Elimina el elemento del array
+        objeto.splice(index, 1); // Elimina el elemento del array
         updateTable(); // Actualiza la tabla después de la eliminación
     }
 }
 
 //esta funcion nos permite editar el producto directamente en la tabala
 function editProduct(id, nameCell, quantityCell, priceCell, editButton) {
+
     // Habilita la edición de los campos de nombre, cantidad y precio
     nameCell.contentEditable = true;
     quantityCell.contentEditable = true;
     priceCell.contentEditable = true;
-    
+
     // Cambia el botón "Editar" a "Guardar Cambios"
     editButton.innerHTML = "Guardar Cambios";
     editButton.className = "saveButton"
     editButton.removeEventListener("click", editProduct);
+
     editButton.addEventListener("click", () => {
         saveProduct(id, nameCell, quantityCell, priceCell, editButton);
     });
@@ -98,34 +161,37 @@ function editProduct(id, nameCell, quantityCell, priceCell, editButton) {
 //funcion validar los cambios en las características del producto y guardarlos en el array
 function saveProduct(id, nameCell, quantityCell, priceCell, editButton) {
     // Guarda los cambios en el array de productos
-    
-    const product = camisetasArray.find(product => product.id === id);
+
+    const product = objeto.find(product => product.id === id);
     if (product) {
         product.nombre = nameCell.textContent;
+    }else{
+        alert(">El campo nombre no puede estar vacío")
     }
-    
+
     const quantity = parseInt(quantityCell.textContent)
-    if (!isNaN(quantity)) {
+    if (isNaN(quantity) || quantity <= 0) {
+        alert("Cantidad no válida");
+        return;
+    } else {
         product.cantidad = quantity;
-    } else {
-        alert("Cantidad no válida")
-        return
     }
-    
+
     const price = parseFloat(priceCell.textContent);
-    if (!isNaN(price)) {
-        product.precio = price
+
+    if (isNaN(price) || price <= 0) {
+        alert("Precio no válido");
+        return;
     } else {
-        alert("Precio no válido")
-        return
+        product.precio = price;
     }
-    
+
     if (product && quantity && price) {
         // Deshabilita la edición de los campos
         nameCell.contentEditable = false;
         quantityCell.contentEditable = false;
         priceCell.contentEditable = false;
-        
+
         // Cambia el botón de "Guardar Cambios" a "Editar"
         editButton.innerHTML = "Editar";
         editButton.className = "tableButton"
@@ -133,80 +199,33 @@ function saveProduct(id, nameCell, quantityCell, priceCell, editButton) {
         editButton.addEventListener("click", () => {
             editProduct(id, nameCell, quantityCell, priceCell, editButton);
         });
+        return;
     }
 }
 
 
 // Función para buscar un producto por su nombre
 function searchProduct() {
-    const searchInput = document.getElementById("searchInput").value.toLowerCase();
-    
-    const foundProducts = camisetasArray.filter(product => product.nombre.toLowerCase().includes(searchInput));
+    const searchInput = document.getElementById("searchInput").value.trim().toLowerCase();
+
+    // Verificar si el campo de entrada está vacío
+    if (searchInput === "") {
+        alert("Por favor, ingrese un término de búsqueda.");
+        return;
+    }
+
+    const foundProducts = objeto.filter(product => product.nombre.toLowerCase().includes(searchInput));
 
     if (foundProducts.length > 0) {
         const productId = foundProducts.map(product => product.id);
         const productName = foundProducts.map(product => product.nombre);
         const productQuantity = foundProducts.map(product => product.cantidad);
         const productPrice = foundProducts.map(product => product.precio);
-        
+
         alert(`Productos encontrados:\nId: ${productId}     Nombre: ${productName}     Cantidad: ${productQuantity}     Precio: ${productPrice}\n`);
     } else {
         alert("No se encontraron productos con ese nombre.");
     }
-    
+
     searchInput.value = "";
-}
-
-function updateTable() {
-    const tableBody = document.getElementById("table-body");
-    tableBody.innerHTML = ""; // Limpiar la tabla
-
-    // Recorrer el array de camisetas y agregar cada una a la tabla
-    camisetasArray.forEach(element => {
-        const row = document.createElement("tr");
-        const idCell = document.createElement("td");
-        const nameCell = document.createElement("td");
-        const quantityCell = document.createElement("td");
-        const priceCell = document.createElement("td");
-
-        const editCell = document.createElement("td");
-        editCell.className = "cellButton"
-        const editButton = document.createElement("button");
-        editButton.innerHTML = "Editar";
-        editButton.className = "tableButton"
-
-        const delCell = document.createElement("td");
-        delCell.className = "cellButton"
-        const delButton = document.createElement("button");
-        delButton.innerHTML = "Eliminar";
-        delButton.className = "tableButton"
-
-        idCell.textContent = element.id;
-        nameCell.textContent = element.nombre;
-        quantityCell.textContent = element.cantidad;
-        priceCell.textContent = element.precio;
-
-        // Agregar evento para el botón de editar
-        editButton.addEventListener("click", () => {
-            editProduct(element.id, nameCell, quantityCell, priceCell, editButton);
-        });
-
-        // Agregar evento para el botón de eliminar
-        delButton.addEventListener("click", () => {
-            const id = element.id;
-            deleteProduct(id)
-        });
-
-        editCell.appendChild(editButton);
-        delCell.appendChild(delButton);
-
-        row.appendChild(idCell);
-        row.appendChild(nameCell);
-        row.appendChild(quantityCell);
-        row.appendChild(priceCell);
-        row.appendChild(editCell);
-        row.appendChild(delCell);
-
-        tableBody.appendChild(row);
-    });
 }
--- scripts/productos.js
+++ scripts/productos.js
@@ -2,7 +2,7 @@ const getRandomQuantity = () =>{
     return Math.floor(Math.random() * 30)+1;
 };
 
-const camisetasArray = [
+const objeto = [
 {id: 1, nombre: "Sarten", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(50.00) + "€"},
 {id: 2, nombre: "Tornillo M4", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(0.50) + "€"},
 {id: 3, nombre: "Cuchillo ", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(10.00) + "€"},
@@ -11,4 +11,4 @@ const camisetasArray = [
 {id: 6, nombre: "Cinta Americana", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(5.00) + "€"},
 {id: 7, nombre: "Destornillador Estrella", cantidad: parseInt(getRandomQuantity()), precio: parseFloat(8.00) + "€"},
 ];
-export {camisetasArray};
+export {objeto};
